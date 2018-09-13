f8911326419d214994cc9882178365bb674fa877
Enio <Enio@DESKTOP-PKUGGK2>
2018-09-13 16:37:40 +0200

    Add project files.

---
+++ Steganography/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Steganography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OpenImageFileButton = new System.Windows.Forms.Button();
+            this.OpenTextFileButton = new System.Windows.Forms.Button();
+            this.EncodeButton = new System.Windows.Forms.Button();
+            this.DecodeButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ImageFilePath = new System.Windows.Forms.TextBox();
+            this.TextFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OpenImageFileButton
+            // 
+            this.OpenImageFileButton.Location = new System.Drawing.Point(456, 41);
+            this.OpenImageFileButton.Name = "OpenImageFileButton";
+            this.OpenImageFileButton.Size = new System.Drawing.Size(35, 24);
+            this.OpenImageFileButton.TabIndex = 1;
+            this.OpenImageFileButton.Text = "...";
+            this.OpenImageFileButton.UseVisualStyleBackColor = true;
+            this.OpenImageFileButton.Click += new System.EventHandler(this.OpenImageFileButton_Click);
+            // 
+            // OpenTextFileButton
+            // 
+            this.OpenTextFileButton.Location = new System.Drawing.Point(456, 93);
+            this.OpenTextFileButton.Name = "OpenTextFileButton";
+            this.OpenTextFileButton.Size = new System.Drawing.Size(35, 24);
+            this.OpenTextFileButton.TabIndex = 2;
+            this.OpenTextFileButton.Text = "...";
+            this.OpenTextFileButton.UseVisualStyleBackColor = true;
+            this.OpenTextFileButton.Click += new System.EventHandler(this.OpenTextFileButton_Click);
+            // 
+            // EncodeButton
+            // 
+            this.EncodeButton.Location = new System.Drawing.Point(272, 200);
+            this.EncodeButton.Name = "EncodeButton";
+            this.EncodeButton.Size = new System.Drawing.Size(75, 23);
+            this.EncodeButton.TabIndex = 3;
+            this.EncodeButton.Text = "Encode";
+            this.EncodeButton.UseVisualStyleBackColor = true;
+            this.EncodeButton.Click += new System.EventHandler(this.EncodeButton_Click);
+            // 
+            // DecodeButton
+            // 
+            this.DecodeButton.Location = new System.Drawing.Point(353, 200);
+            this.DecodeButton.Name = "DecodeButton";
+            this.DecodeButton.Size = new System.Drawing.Size(75, 23);
+            this.DecodeButton.TabIndex = 4;
+            this.DecodeButton.Text = "Decode";
+            this.DecodeButton.UseVisualStyleBackColor = true;
+            this.DecodeButton.Click += new System.EventHandler(this.DecodeButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(452, 200);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ImageFilePath
+            // 
+            this.ImageFilePath.Location = new System.Drawing.Point(293, 44);
+            this.ImageFilePath.Name = "ImageFilePath";
+            this.ImageFilePath.Size = new System.Drawing.Size(157, 20);
+            this.ImageFilePath.TabIndex = 6;
+            // 
+            // TextFilePath
+            // 
+            this.TextFilePath.Location = new System.Drawing.Point(293, 97);
+            this.TextFilePath.Name = "TextFilePath";
+            this.TextFilePath.Size = new System.Drawing.Size(157, 20);
+            this.TextFilePath.TabIndex = 7;
+            this.TextFilePath.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Image File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Text File";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Image Preview";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(257, 123);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(269, 71);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 235);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextFilePath);
+            this.Controls.Add(this.ImageFilePath);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.DecodeButton);
+            this.Controls.Add(this.EncodeButton);
+            this.Controls.Add(this.OpenTextFileButton);
+            this.Controls.Add(this.OpenImageFileButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Steganography";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button OpenImageFileButton;
+        private System.Windows.Forms.Button OpenTextFileButton;
+        private System.Windows.Forms.Button EncodeButton;
+        private System.Windows.Forms.Button DecodeButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.TextBox ImageFilePath;
+        private System.Windows.Forms.TextBox TextFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Steganography/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Steganography
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenImageFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Dialog = new OpenFileDialog();
+            Dialog.Filter = "Image Files (*.png) | *.png;";
+            Dialog.InitialDirectory = @"C:\";
+            Dialog.RestoreDirectory = true;
+            if (Dialog.ShowDialog() == DialogResult.OK)
+            {
+                ImageFilePath.Text = Dialog.FileName.ToString();
+                pictureBox1.ImageLocation = ImageFilePath.Text;
+
+            }
+        }
+
+        private void OpenTextFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Dialog = new OpenFileDialog();
+            Dialog.Filter = "Txt Files (*.txt) | *.txt";
+            Dialog.InitialDirectory = @"C:\";
+            Dialog.RestoreDirectory = true;
+            if (Dialog.ShowDialog() == DialogResult.OK)
+            {
+                TextFilePath.Text = Dialog.FileName.ToString();
+
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public string getBinNum(int intNumber)
+        {
+            string output = Convert.ToString(intNumber, 2);
+            if (output.Length < 8)
+            {
+                while (output.Length != 8) {
+                    output = output.Insert(0, "0");
+                }
+                return output;
+            }
+            else {
+                return output;
+            }
+        }
+
+
+        public int getIntFromBin(string block) {
+
+
+            return Convert.ToInt32(block, 2);
+        }
+
+        public void GenerateHeader(int NumOfChars, Bitmap image) {
+
+              string binNumOfChars = getBinNum(NumOfChars);
+            int n = 16 - binNumOfChars.Length;
+            for(int i=0; i<n; i++)
+            {
+                binNumOfChars = binNumOfChars.Insert(0, "0");  // N A D O P U N J A V A N J E   N A   1 6   B I T A
+            }
+            textBox1.AppendText("Writing to header...");
+            textBox1.AppendText(Environment.NewLine);
+            for (int i = 0; i < 8; i++)
+            {
+                Color pixel = image.GetPixel(0, i);
+                string binPixel = getBinNum(pixel.B);
+                textBox1.AppendText("Org: ");
+                textBox1.AppendText(binPixel.ToString());
+                var BinNewValueOfPixel = new StringBuilder(binPixel);
+                BinNewValueOfPixel.Remove(6, 1);
+                BinNewValueOfPixel.Insert(6, binNumOfChars[2 * i]);
+                BinNewValueOfPixel.Remove(7, 1);
+                BinNewValueOfPixel.Insert(7, binNumOfChars[2 * i + 1]);
+                int NewValueOfPixel = getIntFromBin(BinNewValueOfPixel.ToString());
+                image.SetPixel(i, 0, Color.FromArgb(pixel.R, pixel.G, NewValueOfPixel));
+                textBox1.AppendText("  New:   ");
+                textBox1.AppendText(getBinNum(image.GetPixel(i, 0).B));
+                textBox1.AppendText(Environment.NewLine);
+            }
+
+
+        }
+
+        public void EncodeMessage(Bitmap image)
+        {
+            StreamReader sr = new StreamReader(TextFilePath.Text); //   D O H V A C A N J E
+            string textFromData = sr.ReadToEnd();                  //      T E K S T A 
+            //textBox1.Text=textFromData;                          //   I Z   D A T O T E K E
+            sr.Close();            
+            StreamWriter sw = new StreamWriter("bynaryOutput.txt");    // G E N E R I R A N J E
+            for (int i = 0; i < textFromData.Length; i++)
+            {                                                          //    B I A R N E 
+                string block = getBinNum(textFromData[i]);             //   D A T O T E K E
+                sw.Write(block);
+
+            }
+            sw.Close();
+
+            StreamReader BinLine = new StreamReader("bynaryOutput.txt");
+            string line = BinLine.ReadToEnd();
+            BinLine.Close();
+            int n = line.Length;
+
+            StreamWriter sw1 = new StreamWriter("bynaryOutput.txt");    // G E N E R I R A N J E
+            for (int k= 0; k < textFromData.Length; k++)
+            {                                                          //    B I A R N E 
+                string block = getBinNum(textFromData[k]);             //   D A T O T E K E
+                sw1.WriteLine(block);
+
+            }
+            sw1.Close();
+
+            int Counter = image.Width;
+            int PxCounter = 0;
+            textBox1.AppendText("Duljina bin datoteke: ");
+            textBox1.AppendText(n.ToString());
+            textBox1.AppendText(" znakova");
+            textBox1.AppendText(Environment.NewLine);            
+            StreamReader sr1 = new StreamReader("bynaryOutput.txt");
+            int z = image.Width * image.Height;
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("WRITING TO BODY");
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("---------------------------");
+            textBox1.AppendText(Environment.NewLine);
+            for (int i=0; i<n/8; i++)
+            {
+                                             
+                string binValueOfCharacter = sr1.ReadLine();               
+                for (int j = 0; j < 4; j++)
+                {
+                    int x = Counter % image.Width;
+                    int y = Counter / image.Width;
+                    Color Pixel = image.GetPixel(x, y);
+                    string binValueOfPixel = getBinNum(image.GetPixel(x, y).B);
+                    //textBox1.AppendText("Org: " + binValueOfPixel.ToString());
+                    var ColorBuilder = new StringBuilder(binValueOfPixel);
+                    ColorBuilder.Remove(6, 1);
+                    ColorBuilder.Insert(6, binValueOfCharacter[2 * j]);
+                    ColorBuilder.Remove(7, 1);
+                    ColorBuilder.Insert(7, binValueOfCharacter[2 * j+1]);
+                    //textBox1.AppendText("    New: " + ColorBuilder.ToString());
+                    //textBox1.AppendText(Environment.NewLine);
+                    int NewValueOfColor = getIntFromBin(ColorBuilder.ToString());
+                    image.SetPixel(x, y, Color.FromArgb(Pixel.R,Pixel.G, NewValueOfColor));
+                    Counter++;
+                    PxCounter++;
+                }
+
+                
+
+            }
+            textBox1.AppendText(" Obradeno "+PxCounter.ToString()+" px");
+        }
+        
+
+        private void EncodeButton_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(TextFilePath.Text);
+            string text = sr.ReadToEnd();  
+            SaveFileDialog saveImage = new SaveFileDialog();
+            saveImage.ShowDialog(); 
+            saveImage.Filter= "Png Files (*.png) | *.png";
+            saveImage.DefaultExt = "png";
+            saveImage.AddExtension = true;
+            Bitmap image = new Bitmap(ImageFilePath.Text);
+            GenerateHeader(text.Length, image);
+            EncodeMessage(image);
+            image.Save(saveImage.FileName);
+            
+        }
+
+        private void DecodeButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ShowDialog();
+            sfd.Filter = "Txt Files (*.txt) | *.txt";
+            sfd.DefaultExt = "txt";
+            sfd.AddExtension = true;
+            Bitmap image = new Bitmap(ImageFilePath.Text);
+            textBox1.AppendText("---------------------------");
+            textBox1.AppendText(Environment.NewLine);
+            string BinHeader = "";
+            for (int i = 0; i < 8; i++) {
+                string binPixel = getBinNum(image.GetPixel(i, 0).B);
+                BinHeader = BinHeader.Insert(BinHeader.Length, binPixel[6].ToString());
+                BinHeader = BinHeader.Insert(BinHeader.Length, binPixel[7].ToString());
+            }
+            string message = "";
+            int n = getIntFromBin(BinHeader);
+            textBox1.AppendText("INFO FROM HEADER:");
+            textBox1.AppendText(Environment.NewLine);            
+            textBox1.AppendText("This image contains ");
+            textBox1.AppendText(getIntFromBin(BinHeader).ToString());
+            textBox1.AppendText(" characters");
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("---------------------------");
+            textBox1.AppendText(Environment.NewLine);
+            int counter = image.Width;
+            StreamWriter sw = new StreamWriter(sfd.FileName);
+            string character = "";
+            for(int i=0; i<n; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int x = counter % image.Width;
+                    int y = counter / image.Width;
+                    string binValueOfPixel = getBinNum(image.GetPixel(x, y).B);
+                   character=character.Insert(character.Length, binValueOfPixel[6].ToString());
+                   character=character.Insert(character.Length, binValueOfPixel[7].ToString());                    
+                    counter++;
+
+                }
+                
+                int AsciiCharacter = getIntFromBin(character);                
+                char c = (char)AsciiCharacter;
+                message=message.Insert(message.Length, c.ToString());
+                sw.Write(c);
+                character = "";
+                //textBox1.AppendText(c.ToString());
+                
+            }
+            sw.Close();
+        }
+    }
+    }
+
+
+
+   
+
+
